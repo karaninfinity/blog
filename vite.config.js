e5a1823dd84472ae89f70c2e66df61578fb9a31a
karanharsora12 <karanharsora2@gmail.com>
2024-07-22 23:52:04 +0530

    latest chages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/my-app/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
