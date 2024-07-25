cf772fd26b7023054fe03f598fd3a02c11dd4e60
Vimal Kava <karan.infinitysoftech@gmail.com>
2024-07-25 15:03:47 +0530

    latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-app/",
+  base: "/blog/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
